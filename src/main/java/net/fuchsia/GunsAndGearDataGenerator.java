95e177abf9a717845bd0e7b536495b476d274f36
Jayson <jaysonjson@web.de>
2024-04-08 00:16:59 +0100

    init

---
+++ src/main/java/net/fuchsia/GunsAndGearDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fuchsia;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GunsAndGearDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
